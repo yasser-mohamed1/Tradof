ff51df3c146e349e58a83b54804d12d00ebe2685
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2025-01-31 04:15:53 +0200

    setting up proposal module infrastructure

---
+++ Tradof.EntityFramework/Migrations/20250131021352_test.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tradof.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CompanyId",
+                table: "Languages",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_CompanyId",
+                table: "Languages",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Languages_Companies_CompanyId",
+                table: "Languages",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Languages_Companies_CompanyId",
+                table: "Languages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Languages_CompanyId",
+                table: "Languages");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "Languages");
+        }
+    }
+}
